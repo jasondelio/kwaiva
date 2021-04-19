0309c85b0754d7f8694c8a1f4dbbe29f82762601
jasondelio <63277002+jasondelio@users.noreply.github.com>
2021-04-19 12:21:54 +1000

    Sidebar & Topbar added (not finished yet)

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import "./Sidebar.css"
+import { BiMusic } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
+import {CgNotes} from "react-icons/cg"
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="Sidebar">
+            <img src="/Assets/kwaiva_logo.png" alt=""/>
+            <ul className="SidebarList">
+                <li className = "row" onClick = {() => { window.location.pathname = "/songs"}}>
+                    <div className = "icon"><BiMusic/></div>
+                    <div className = "title">SONGS</div>
+                </li>
+                <li className = "row" onClick = {() => { window.location.pathname = "/users"}}>
+                    <div className = "icon"><FaRegUser/></div>
+                    <div className = "title">USERS</div>
+                </li>
+                <li className = "row" onClick = {() => { window.location.pathname = "/request"}}>
+                    <div className = "icon" ><CgNotes/></div>
+                    <div className = "title">REQUEST</div>
+                </li>
+            </ul>
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
