9e79a3b8a63c081678ebfccd326d53e4b355b193
RyutaKitai <ryuuta.k@icloud.com>
2021-04-28 04:04:39 +1000

    add router & login verification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
-import LoginPage from './Pages/LoginPage'
+import LoginPage from './Pages/LoginPage';
 import SongsPage from './Pages/SongsPage';
-import UsersPage from './Pages/UsersPage'
+import UsersPage from './Pages/UsersPage';
+
 function App() {
   return (
-    <UsersPage/>
+    <BrowserRouter>
+      <Route exact path="/" component={LoginPage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/songs" component={SongsPage} />
+      <Route path="/users" component={UsersPage} />
+    </BrowserRouter>
+    // <React.Fragment>
+    //   <Sidebar />
+    //   <Topbar />
+    //   <LoginPage />
+    // </React.Fragment>
   );
 }
 
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginPage.css"
 
+function handleVerification(username, password) {
+  var bcrypt = require('bcryptjs');
+  const saltRounds = 10;
+  const tempPassword = 'passwordTempTempTafa';
+  var hash = bcrypt.hashSync(tempPassword, saltRounds);
+  var isValid = bcrypt.compareSync(password, hash);
+  if (isValid && username == "admin") {
+    window.location.pathname = "/songs"
+  }
+  else {
+    window.location.pathname = "/login"
+  }
+}
+
 function LoginPage(params) {
+  const [username, setUsername] = useState('');
+  const [passwordss, setPassword] = useState('');
+
   return (
     <div className="LoginPage">
       <div className="img_con">
@@ -9,9 +26,9 @@ function LoginPage(params) {
       </div>
       <div className="container">
         <h1>ADMIN LOGIN</h1>
-        <input className="inputbox" placeholder = "USERNAME OR EMAIL" />
-        <input type="password" className="inputbox" placeholder = "PASSWORD" />
-        <a className="login" onClick={() => { window.location.pathname = "/songs" }}>LOGIN</a>
+        <input className="inputbox" placeholder="USERNAME OR EMAIL" onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" className="inputbox" placeholder="PASSWORD" onChange={(e) => setPassword(e.target.value)} />
+        <a className="login" onClick={() => handleVerification(username, passwordss)}>LOGIN</a>
       </div>
     </div>
   );
