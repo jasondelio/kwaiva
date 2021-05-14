40d445976c6e54e035e18e0e8b297b49bc94caee
Arin Kim <arin.kim0801@gmail.com>
2021-05-14 19:50:29 +1000

    User page, needs to fix error

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ var upload = multer({
   limits: { fieldSize: 25 * 1024 * 1024 }
 })
 
+app.set('port', process.env.PORT || 3001);
+
+app.use(expressCspHeader({
+  directives: {
+      'script-src': [SELF, INLINE],
+  }
+}));
+
 const connection = mysql.createConnection(config);
 
 connection.connect(function (err) {
@@ -137,6 +145,110 @@ app.post('/deletesong', (req, res) => {
   })
 })
 
-app.listen(3001, () => {
-  console.log("running server")
-})
+/*User*/
+/*const [userInfo, setuserInfo] = React.useState({
+        fstName: '',
+        lstName: '',
+        userName: '',
+        email: '',
+        passwords: '',
+        role: ''
+    });*/
+
+    app.get('/users/get', (req, res) => {
+      connection.query('select * from Users;', (err, result) => {
+        res.send(result)
+      })
+    })
+  
+    // Get data of selected user
+    app.get('/users/getUser', (req, res) => {
+      const GET = 'select * from Users where keyId=?;';
+      var id = req.query.keyid;
+      console.log('Post a Customer: ' + JSON.stringify(req.query));
+      connection.query(GET,[id], (err, result) => {
+        console.log('result: ' + JSON.stringify(result));
+        res.send(result)
+      })
+    })
+  
+    // Get data of selected user
+    app.get('/users/getUserEmail', (req, res) => {
+      const GET = 'select * from Users where email=?;';
+      var id = req.query.email;
+      console.log('Post a Customer: ' + JSON.stringify(req.query));
+      connection.query(GET,[id], (err, result) => {
+        res.send(result)
+      })
+    })
+  
+  // INSERT statment, data = [(username, email, lastname, firstname, role)]
+  app.post('/users/insert', (req, res) => {
+    // update statment, data = [tableName, "conlumnName2 = 'NewData'", "conlumnName2 = 'OldData'"]
+    const INSERT = 'INSERT INTO Users (firstname, lastname, username, password, email, role) VALUES (?,?,?,?,?,?)';
+    var fstName = req.body.firstName;
+    var lstName = req.body.lastName;
+    var userName = req.body.userName;
+    var email = req.body.email;
+    var passwords = req.body.password;
+    var role = req.body.role;
+    console.log("%s",connection.state)
+  
+    console.log('Post a Customer: ' + JSON.stringify(req.body));
+  
+    connection.query(INSERT, [fstName,lstName,userName, passwords, email, role] , (err, result) => {
+      console.log(err);
+      console.log(result);
+      res.send(result)
+    })
+  })
+  
+  
+  app.post('/users/update', (req, res) => {
+    // update statment, data = [tableName, "conlumnName2 = 'NewData'", "targetconlumnName = 'targetValue'"]
+    const UPDATE = "UPDATE Users SET firstName = ?, lastName = ?, userName = ?,password = ?,email =?,role =? WHERE keyId = ?;"
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var userName = req.body.userName;
+    var email = req.body.email;
+    var password = req.body.password;
+    var role = req.body.role;
+    var keyId = req.body.keyId;
+  
+    console.log(req.body)
+  
+    connection.query(UPDATE, [firstName,lastName,userName, password, email, role, keyId], (err, result) => {
+      console.log(err);
+      console.log(result);
+      res.send(result)
+    })
+  })
+  
+  app.post('/users/delete', (req, res) => {
+    // update statment, data = [tableName, "conlumnName2 = 'NewData'", "targetconlumnName = 'targetValue'"]
+    const DELETE = "DELETE from Users where keyId = ?;"
+  
+    console.log(req.body)
+  
+    connection.query(DELETE, [keyId], (err, result) => {
+      console.log(err);
+      console.log(result);
+      res.send(result)
+    })
+  })
+  
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  
+  var server = app.listen(process.env.PORT, () => {
+    console.log("running server")
+    var host = server.address().address
+    var port = server.address().port
+   
+    console.log("App listening at http://%s:%s and %s", host, port, connection.state)
+  })
--- src/Pages/UsersPage.js
+++ src/Pages/UsersPage.js
@@ -1,12 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
 import "./UsersPage.css"
 import Sidebar from "../Components/Sidebar";
 import Topbar from "../Components/Topbar";
-import {UsersMockData} from "./UsersMockData"
 import Modal from 'react-bootstrap/Modal'
 import { IoClose } from "react-icons/io5";
+import Axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 function AddUserPage(props) {
+    const history = useHistory();
+    const [userInfo, setuserInfo] = React.useState({
+        firstName: '',
+        lastName: '',
+        userName: '',
+        email: '',
+        password: '',
+        role: ''
+    });
+
+    const handleChange = ((e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        setuserInfo({ ...userInfo, [name]: value });
+        console.log(userInfo)
+    })
+    
+
+    const handleSubmit = ((event) => {
+        event.preventDefault()
+        console.log("connect")
+        console.log(userInfo)
+        
+        // let formData = new FormData(userInfo)
+        // console.log(userInfo)
+        let Userdata = new FormData();
+
+        for (var pair of Userdata.entries()) {
+            console.log(pair[0] + ', ' + pair[1])
+        }
+        Userdata.append('firstName', userInfo.firstName);
+        Userdata.append('lastName', userInfo.lastName);
+        Userdata.append('userName', userInfo.userName);
+        Userdata.append('email', userInfo.email);
+        Userdata.append('password', userInfo.password);
+        Userdata.append('role', userInfo.role);
+        console.log(Userdata.get('firstName'))
+        Axios({
+            method: 'POST',
+            url: "http://localhost:3001/users/insert",
+            data: Userdata,
+            headers: { "Content-Type": "multipart/form-data" },
+        }).then(function (response) {
+            //handle success
+            console.log(response);
+
+        })
+        .catch(function (response) {
+            //handle error
+            console.log(response);
+        });
+
+        history.push('/users');
+    });
+
     return (
         <Modal
         className = "ModalPage"
@@ -21,33 +78,33 @@ function AddUserPage(props) {
             </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <form>
-                <p>
+            <form action="/users/insert" method="POST" onSubmit={(e) => handleSubmit(e)}>
+            <p>
                     First Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
-                    <input type = "text"/>
+                    <input type = "text" name="firstName" onChange={e => handleChange(e)}/>
                 </p>
                 <p>
                     Last Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
-                    <input type = "text"/>
+                    <input type = "text" name="lastName" onChange={e => handleChange(e)}/>
                 </p>
                 <p>
                     Username&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
-                    <input type = "text"/>
+                    <input type = "text" name="userName" onChange={e => handleChange(e)}/>
                 </p>
                 <p>
                     Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
-                    <input type = "email"/>
+                    <input type = "email" name="email" onChange={e => handleChange(e)}/>
                 </p>
                 <p>
                     Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
-                    <input type = "password"/>
+                    <input type = "password" name="password" onChange={e => handleChange(e)}/>
                 </p>
                 <p>
                     Role&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
-                    <select name="role" id="role">
-                        <option value="admin">Admin</option>
-                        <option value="fans">Fans</option>
-                        <option value="musician">Musician</option>
+                    <select id="role" name="role" onClick={e => handleChange(e)}>
+                        <option value="admin" name="role" onClick={e => handleChange(e)}>Admin</option>
+                        <option value="fans" name="role" onClick={e => handleChange(e)}>Fans</option>
+                        <option value="musician" name="role" onClick={e => handleChange(e)}>Musician</option>
                     </select>
                 </p>
                 <input type="submit" value="SUBMIT"></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -58,22 +115,163 @@ function AddUserPage(props) {
     )
 }
 
+function EditUserPage(props) {
+    const [selectedData, setSelectedData] = useState({
+        firstName: props.userData.firstName,
+        lastName: props.userData.lastName,
+        userName: props.userData.userName,
+        email: props.userData.email,
+        password: props.userData.password,
+        role: props.userData.role
+    });
+    const handleChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        if(value === null) {
+            value = `props.userData.${name}`;
+        }
+        setSelectedData({ ...selectedData, [name]: value });
+    }
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // console.log(selectedData)
+        let EditedUserdata = new FormData();
+
+        for (var pair of EditedUserdata.entries()) {
+            console.log(pair[0] + ', ' + pair[1])
+        }
+        EditedUserdata.append('firstName', selectedData.firstName);
+        EditedUserdata.append('lastName', selectedData.lastName);
+        EditedUserdata.append('userName', selectedData.userName);
+        EditedUserdata.append('email', selectedData.email);
+        EditedUserdata.append('password', selectedData.password);
+        EditedUserdata.append('role', selectedData.role);
+        EditedUserdata.append('keyId', props.userData.keyId);
+        var server = 3001;
+        Axios({
+            method: 'POST',
+            url: `http://localhost:${server}/users/update`,
+            data: EditedUserdata,
+            headers: { "Content-Type": "multipart/form-data" },
+        }).then(function (response) {
+            //handle success
+            console.log(response);
+
+        })
+        .catch(function (response) {
+            //handle error
+            console.log(response);
+        });
+    };
+
+    return (
+        <Modal
+        className = "ModalPage"
+        {...props}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header className = "header">
+            <IoClose className = "close" size={30} onClick={props.onHide}/>
+            <Modal.Title>
+                <h4>{props.title}</h4> 
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <form action="/users/update" method="POST" onSubmit={(e) => { handleSubmit(e); props.onHide();}}>
+            <p>
+                    First Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+                    <input type = "text" name="firstName" defaultValue={selectedData.firstName} onChange={e => handleChange(e)}/>
+                </p>
+                <p>
+                    Last Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+                    <input type = "text" name="lastName" defaultValue={selectedData.lastName} onChange={e => handleChange(e)}/>
+                </p>
+                <p>
+                    Username&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+                    <input type = "text" name="userName" defaultValue={selectedData.userName} onChange={e => handleChange(e)}/>
+                </p>
+                <p>
+                    Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+                    <input type = "email" name="email" defaultValue={selectedData.email} onChange={e => handleChange(e)}/>
+                </p>
+                <p>
+                    Password&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+                    <input type = "password" name="password" defaultValue={selectedData.password} onChange={e => handleChange(e)}/>
+                </p>
+                <p>
+                    Role&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
+                    <select id="role" name="role" onChange={e => handleChange(e)}>
+                        <option value="admin" name="role" onChange={e => handleChange(e)}>Admin</option>
+                        <option value="fans" name="role" onChange={e => handleChange(e)}>Fans</option>
+                        <option value="musician" name="role" onChange={e => handleChange(e)}>Musician</option>
+                    </select>
+                </p>
+                <button type="edit" value="EDIT">EDIT</button>
+            </form>
+        </Modal.Body>
+      </Modal>
+    )
+}
+
 function UsersPage() {
-    const [searchTerm, setSearchTerm] = React.useState("");
-    const [searchResults, setSearchResults] = React.useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [userdata, setUserData] = useState([]);
+    const [modalShow, setModalShow] = useState(false);
+    const [modalUpdateShow, setModalUpdateShow] = useState(false);
+
     const handleChange = event => {
        setSearchTerm(event.target.value);
      };
-    React.useEffect(() => {
-       const results = UsersMockData.filter(user =>
-         user.username.toLocaleLowerCase().includes(searchTerm) ||
-         user.lastname.toLocaleLowerCase().includes(searchTerm) ||
-         user.firstname.toLocaleLowerCase().includes(searchTerm) ||
+
+    //  const handleSelection = (e) => {
+    //     var user = new FormData();
+    //     user.append('keyid', 2);
+    //     for (var pair of user.entries()) {
+    //         console.log(pair[0] + ', ' + pair[1])
+    //     }
+    //     Axios({
+    //         method: "GET",
+    //         url: "http://localhost:3001/users/getUser",
+    //         params: { keyid: e },
+    //         headers: { "Content-Type": "multipart/form-data" },
+    //     })
+    //         .then(
+    //             (res)  =>{
+    //                 var d = res.data
+    //             return setSelectedData(d);
+                
+    //         })
+    //         .catch(function (response) {
+    //             //handle error
+    //             console.log(response);
+    //         });
+    //  };
+     //for getting data from db
+     useEffect(() => {
+        Axios.get('http://localhost:3001/users/get').then((res) => {
+            var data = res.data;
+            const results = data.map(song =>
+                song
+            );
+            setUserData(results);
+        })
+    }, [userdata])
+     // for search
+    useEffect(() => {
+       const results = userdata.filter(user =>
+         user.userName.toLocaleLowerCase().includes(searchTerm) ||
+         user.lastName.toLocaleLowerCase().includes(searchTerm) ||
+         user.firstName.toLocaleLowerCase().includes(searchTerm) ||
          user.email.toLocaleLowerCase().includes(searchTerm)
        );
        setSearchResults(results);
-     }, [searchTerm]);
-    const [modalShow, setModalShow] = React.useState(false);
+     }, [searchResults, searchTerm, userdata]);
+     
     return (
         <React.Fragment>
             <Sidebar/>
@@ -101,14 +299,23 @@ function UsersPage() {
                         <th>Role</th>
                     </tr>
                     {searchResults.map((user, index) =>(
-                        <tr className = "list" key = {index}>
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
-                            <td>{user.firstname}</td>
-                            <td>{user.lastname}</td>
-                            <td>{user.role}</td>
+                        <tr className = "list" key = {index} onClick={() => {setModalUpdateShow(true)}}>
+                            <td id={user.keyId} >{user.userName}</td>
+                            <td id={user.keyId} >{user.email}</td>
+                            <td id={user.keyId} >{user.firstName}</td>
+                            <td id={user.keyId} >{user.lastName}</td>
+                            <td id={user.keyId} >{user.role}</td>
+                            <EditUserPage
+                        show={modalUpdateShow}
+                        onHide={() => setModalUpdateShow(false)}
+                        title = "Edit USER"
+                        userData = {user}
+                        />
                         </tr>
+                        
                     ))}
+                    
+                    
                 </table>
             </div>
         </React.Fragment>
